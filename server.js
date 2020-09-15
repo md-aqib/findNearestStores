4081a04bf4f5d8fb5984251b0fefbe2923c7b658
aquibmax <mdaquib327@gmail.com>
2020-09-15 23:19:29 +0530

    some changes in server file

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
 require("dotenv").config();
+// require('dotenv').config({ debug: process.env.DEBUG })
+const bodyParser = require("body-parser");
+const passport = require("passport");
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+let port = process.env.PORT || 5000;
 
-let port = process.env.PORT || 3000;
+/*A new body object containing the parsed data is populated on the request object after the middleware (i.e. req.body).
+This object will contain key-value pairs, where the value can be a string or array (when extended is false),
+or any type (when extended is true).*/
+app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+app.use(bodyParser.json({ limit: "50mb" }));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 let mongoOptions = {
   useNewUrlParser: true,
@@ -18,7 +26,7 @@ let mongoOptions = {
 };
 mongoose
   .connect(process.env.MONGO_URI, mongoOptions)
-  .then(() => console.log("MongoDB connected Successfully"))
+  .then(() => console.log("MongoDB connected Successfully..."))
   .catch((err) => console.log("Error:", err));
 
 app.use(morgan("dev"));
